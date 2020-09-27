33f60e27309b4fc31e14dd7ba932f65cb4809377
20192170 <e.weldeghebreal@student.tue.nl>
2020-09-27 21:48:04 +0200

    Bat
    Vleermuis beweegt van links naar rechts, wordt nog niet geflipped.

--- Skyscraper Waves/Assets/Bat.cs
+++ Skyscraper Waves/Assets/Bat.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class Bat : MonoBehaviour
 {
-    public float speed;
-    public bool MoveRight;
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(MoveRight){
-            transform.Translate(2 * Time.deltaTime * speed , 0.0);
-        }
-        else{
-            transform.Translate(-2 * Time.deltaTime * speed , 0.0);
-        }
-    }
+    public bool dirRight = true;
+    public float speed = 2.0f;
+    void Start ()
+   {
+        // get a reference to the SpriteRenderer component on this gameObject
+        //mySpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+   }
+     void Update () {
+         if (dirRight)
+             transform.Translate (Vector2.right * speed * Time.deltaTime);
+         else
+             transform.Translate (-Vector2.right * speed * Time.deltaTime);
+         
+         if(transform.position.x >= 4.0f) {
+             dirRight = false;
+             //mySpriteRenderer.flipx = false;
+         }
+         
+         if(transform.position.x <= -4) {
+             dirRight = true;
+         }
+}
 }
