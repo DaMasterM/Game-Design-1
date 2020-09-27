400bccdeee171384611b91ca51c070df286491de
20192170 <e.weldeghebreal@student.tue.nl>
2020-09-27 20:50:09 +0200

    Bat enemy

---
+++ Skyscraper Waves/Assets/Bat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : MonoBehaviour
+{
+    public float speed;
+    public bool MoveRight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(MoveRight){
+            transform.Translate(2 * Time.deltaTime * speed , 0.0);
+        }
+        else{
+            transform.Translate(-2 * Time.deltaTime * speed , 0.0);
+        }
+    }
+}
