138ad8a317dc73a7e56af5fd29da902cfcc2a5af
20192170 <e.weldeghebreal@student.tue.nl>
2020-09-28 15:15:02 +0200

    Bat
    Bat is now a prefab and moves horizontally and flips at edges

--- Skyscraper Waves/Assets/Scripts/Bat.cs
+++ Skyscraper Waves/Assets/Scripts/Bat.cs
@@ -6,10 +6,12 @@ public class Bat : MonoBehaviour
 {
     public bool dirRight = true;
     public float speed = 2.0f;
+    public SpriteRenderer renderer;
     void Start ()
    {
         // get a reference to the SpriteRenderer component on this gameObject
         //mySpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
    }
      void Update () {
          if (dirRight)
@@ -19,11 +21,12 @@ public class Bat : MonoBehaviour
          
          if(transform.position.x >= 4.0f) {
              dirRight = false;
-             //mySpriteRenderer.flipx = false;
+            renderer.flipX = true;
          }
          
          if(transform.position.x <= -4) {
              dirRight = true;
+             renderer.flipX = false;
          }
 }
 }
