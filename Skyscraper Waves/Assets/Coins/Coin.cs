89bd618296b41a316c8ed9c15e39da19c5aaeff9
ymkeverspui <y.r.verspui@student.tue.nl>
2020-10-01 17:05:25 +0200

    coins

--- Skyscraper Waves/Assets/Coins/Coin.cs
+++ Skyscraper Waves/Assets/Coins/Coin.cs
@@ -4,20 +4,11 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
- public int coinValue = 1; // als de player tegen een coin aanloopt verandert de score
- 
- void OnTriggerEnter2D(Collider2D other){
-	  if(other.gameObject.CompareTag("Player")){ 
-			CoinCounter.instance.ChangeScore(coinValue);
-	  }
+ private void OnTriggerEnter2D(Collider2D other)
+ {
+      if (other.gameObject.CompareTag("Coin"))
+      {
+            Destroy(other.gameObject);
+      }
  }
- 
-
-private void OnTrigerEnter2D(Collider2D other) // als de player tegen de coin aanloopt verwijdert het spel de coin
-{ 
-	if(other.gameObject.CompareTag("Coin")) 
-	{
-		Destroy(other.gameObject);
-		}
-	}
-}	
+    }
