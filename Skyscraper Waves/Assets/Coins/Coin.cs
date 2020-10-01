8fa93894282532130811f080fe857f43be6d8586
ymkeverspui <y.r.verspui@student.tue.nl>
2020-10-01 11:34:56 +0200

    coins

---
+++ Skyscraper Waves/Assets/Coins/Coin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+ public int coinValue = 1; // als de player tegen een coin aanloopt verandert de score
+ 
+ void OnTriggerEnter2D(Collider2D Collision){
+	  if(Other.gameObject.CompareTag("Player"){ 
+			CoinCounter.instance.ChangeScore(coinValue)
+	  }
+ }
+
+private void OnTrigerEnter2D(Collider2D other){ // als de player tegen de coin aanloopt verwijdert het spel de coin
+	if(other.gameObject.CompareTag("Coin"")) {
+		Destroy(other.gameObject);
+		}
+	}
+}
--- Skyscraper Waves/Assets/Coins/CoinCounter.cs
+++ Skyscraper Waves/Assets/Coins/CoinCounter.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CoinCounter : MonoBehaviour
 {
+
+public static   CoinCounter instance;
+public TextMeshProUGUI text;
+    int score;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if instance == null {
+            instace == this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void ChangeScore (int coinValue) {
+        score += coinValue;
+        text.text = "X"+score.ToString(); 
     }
-}
