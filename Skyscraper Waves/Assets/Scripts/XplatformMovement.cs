600a819e60fe4405170ee92d5cb982a8e27f598f
ellabusser <e.busser@student.tue.nl>
2020-10-01 15:21:46 +0200

    platform

---
+++ Skyscraper Waves/Assets/Scripts/XplatformMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XplatformMovement : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+    public float velX = 1.5f;
+    private Vector2 velocity;
+
+    void Start()
+    {
+        velocity = new Vector2(velX, 0.0f);
+    }
+
+    void FixedUpdate()
+    {
+        rb2D.MovePosition(rb2D.position + velocity * Time.fixedDeltaTime);
+        if (rb2D.position.x >= -6.0)
+        {
+            velocity = new Vector2(-velX, 0.0f);
+        }
+        if (rb2D.position.x <= -10.0)
+        {
+            velocity = new Vector2(velX, 0.0f);
+        }
+    }
+}
---
+++ Skyscraper Waves/Assets/Scripts/YplatformMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YplatformMovement : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+    public float velY = 1.5f;
+    private Vector2 velocity;
+
+    void Start()
+    {
+        velocity = new Vector2(0.0f, velY);
+    }
+
+    void FixedUpdate()
+    {
+        rb2D.MovePosition(rb2D.position + velocity * Time.fixedDeltaTime);
+        if (rb2D.position.y >= 8.0)
+        {
+            velocity = new Vector2(0.0f, -velY);
+        }
+        if (rb2D.position.y <= -2.0)
+        {
+            velocity = new Vector2(0.0f, velY);
+        }
+    }
+}
