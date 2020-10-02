5df811d420f3a7cea82ffed2cb63ba00aaf129a5
20192170 <e.weldeghebreal@student.tue.nl>
2020-10-02 21:37:27 +0200

    Bat
    Added damage at collision, no way to test as i can't see the health of the player.

--- Skyscraper Waves/Assets/Standard Assets/2D/Scripts/Bat.cs
+++ Skyscraper Waves/Assets/Standard Assets/2D/Scripts/Bat.cs
@@ -2,44 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bat : MonoBehaviour
+namespace UnityStandardAssets._2D
 {
-    private bool dirRight = true;
-    private float speed = 2.0f;
-    private SpriteRenderer renderer;
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class Bat : MonoBehaviour
+    {
+        private bool dirRight = true;
+        private float speed = 2.0f;
+        private SpriteRenderer renderer;
 
-    //public PlatformerCharacter2D other;
-    void Start ()
-   {
-        // get a reference to the SpriteRenderer component on this gameObject
-        SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
-   }
-    private void Update () {
-         if (dirRight)
-             transform.Translate (Vector2.right * speed * Time.deltaTime);
-         else
-             transform.Translate (-Vector2.right * speed * Time.deltaTime);
-         
-         if(transform.position.x >= 4.0f) {
-             dirRight = false;
-            renderer.flipX = true;
-         }
-         
-         if(transform.position.x <= -4) {
-             dirRight = true;
-             renderer.flipX = false;
-         }
-
-         
+        public PlatformerCharacter2D Player2D;
+        void Start ()
+    {
+            // get a reference to the SpriteRenderer component on this gameObject
+            SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
     }
-
-    private void OnTriggerEnter2D(Collider2D collision){
-            //Check for a match with the specified tag on any GameObject that collides with your GameObject
-            if (collision.gameObject.tag == "Player")
-            {
-                //If the GameObject's tag matches the one you suggest, deal damage
-                renderer.flipY = true;
-                //other.LoseHealth(0.1);
+        private void Update () {
+            if (dirRight)
+                transform.Translate (Vector2.right * speed * Time.deltaTime);
+            else
+                transform.Translate (-Vector2.right * speed * Time.deltaTime);
+            
+            if(transform.position.x >= 4.0f) {
+                dirRight = false;
+                renderer.flipX = true;
             }
+            
+            if(transform.position.x <= -4) {
+                dirRight = true;
+                renderer.flipX = false;
+            }
+
+            
         }
+
+        private void OnTriggerEnter2D(Collider2D collision){
+                //Check for a match with the specified tag on any GameObject that collides with your GameObject
+                if (collision.gameObject.tag == "Player")
+                {
+                    //If the GameObject's tag matches the one you suggest, deal damage
+                    renderer.flipY = true;
+                    Player2D.LoseHealth(1);
+                }
+            }
+    }
 }
