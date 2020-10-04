040f508e8451e8f7867021df9f5603a3899eace4
Wouter <43830356+kabouter1123@users.noreply.github.com>
2020-10-04 15:00:01 +0200

    added other powerups
    no sprite yet thus are still all coins

---
+++ Skyscraper Waves/Assets/Powerups/PowerUpSpeed.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class PowerUpSpeed : PowerUp
+{
+    public float speed = 15f;
+    public float duration = 5f;
+
+    protected override void PowerUpPayload()  // Checklist item 1
+    {
+        base.PowerUpPayload();
+
+        // Payload is to give some health bonus
+        playerBrain.SetMaxSpeed(speed, false);      
+    }
+
+    protected override void PowerUpHasExpired ()     // Checklist item 2
+    {
+        if (powerUpState == PowerUpState.IsExpiring)
+        {
+            return;
+        }
+        playerBrain.SetMaxSpeed(speed, true);
+
+        base.PowerUpHasExpired ();
+    }
+
+    private void Update ()                            // Checklist item 3
+    {
+        if (powerUpState == PowerUpState.IsCollected)
+        {
+            duration -= Time.deltaTime;
+            if (duration < 0)
+            {
+                PowerUpHasExpired ();
+            }
+        }
+    }
+}
---
+++ Skyscraper Waves/Assets/Powerups/PowerUpStrenght.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class PowerUpStrenght : PowerUp
+{
+    public float strength = 100f;
+    public float duration = 5f;
+
+    protected override void PowerUpPayload()  // Checklist item 1
+    {
+        base.PowerUpPayload();
+
+        // Payload is to give some health bonus
+        playerBrain.SetStrength(strength, false);      
+    }
+
+    protected override void PowerUpHasExpired ()     // Checklist item 2
+    {
+        if (powerUpState == PowerUpState.IsExpiring)
+        {
+            return;
+        }
+        playerBrain.SetStrength(strength, true);
+
+        base.PowerUpHasExpired ();
+    }
+
+    private void Update ()                            // Checklist item 3
+    {
+        if (powerUpState == PowerUpState.IsCollected)
+        {
+            duration -= Time.deltaTime;
+            if (duration < 0)
+            {
+                PowerUpHasExpired ();
+            }
+        }
+    }
+}
