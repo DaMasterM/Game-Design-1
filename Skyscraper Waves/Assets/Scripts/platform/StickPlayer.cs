c7df72d3dfabfe3217d6088a5db8823a96a4030a
ellabusser <e.busser@student.tue.nl>
2020-10-05 14:00:00 +0200

    platform fix

---
+++ Skyscraper Waves/Assets/Scripts/platform/StickPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickPlayer : MonoBehaviour
+{
+    private void OnCollisionEnter2D (Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //moving = true;
+            collision.collider.transform.SetParent(transform);  
+            Debug.Log("touch");
+        }
+    }
+
+     private void OnCollisionExit2D (Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.collider.transform.SetParent(null);  
+        }
+    }
+
+    
+}
