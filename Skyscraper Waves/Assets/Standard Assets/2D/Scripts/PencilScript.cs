500bd0f44ec3dc206e478fd40578d28aae8968aa
Wouter <43830356+kabouter1123@users.noreply.github.com>
2020-10-08 16:46:26 +0200

    added everything i did for thursday again

---
+++ Skyscraper Waves/Assets/Standard Assets/2D/Scripts/PencilScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PencilScript : MonoBehaviour
+{
+    public float speed = 3f;
+    public SpriteRenderer renderer;
+
+    public void StartShoot(bool dirRight)
+    {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+
+        if (dirRight) 
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speed,0);
+            renderer.flipX = false;
+        } 
+        else 
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-speed,0);
+            renderer.flipX = true;
+        }
+
+    }
+}
