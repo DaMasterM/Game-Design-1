847dfe05b64ab65cca22f4d6c7c0b1aea95b2744
Wouter <43830356+kabouter1123@users.noreply.github.com>
2020-10-08 14:48:41 +0200

    corrected powerup sprites and added shooting

--- Skyscraper Waves/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Skyscraper Waves/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -35,6 +35,15 @@ namespace UnityStandardAssets._2D
         private Rigidbody2D m_Rigidbody2D;
         private bool m_FacingRight = true;  // For determining which way the player is currently facing.
 
+        //shooting  variables
+        public int ammo = 0; //amount of ammo currently available
+        public GameObject pencil;
+        [SerializeField]
+        Transform pencilSpawnPos;
+        private bool isShooting = false;
+        public float pencilSpeed = 3f;
+        public float shootDelay = 0.5f;
+
         private void Awake()
         {
             // Setting up references.
@@ -234,5 +243,28 @@ namespace UnityStandardAssets._2D
        
         }
 
+        public void Attack() 
+        {
+            if (isShooting) {return;}
+            isShooting = true;
+            if (ammo <= 0) {
+                return;
+            }
+            IncreaseAmmo(-1);
+
+            GameObject p = Instantiate(pencil);
+            p.GetComponent<PencilScript>().StartShoot(m_FacingRight);
+            p.transform.position = pencilSpawnPos.transform.position;
+
+            Invoke("ResetShoot", shootDelay);
+        }
+
+        private void ResetShoot() {
+            isShooting = false;
+        }
+
+        public void IncreaseAmmo(int ammoValue) {
+            ammo += ammoValue;
+        }
     }
 }
