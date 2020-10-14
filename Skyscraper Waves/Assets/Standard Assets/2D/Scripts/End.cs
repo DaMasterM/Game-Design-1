47c6c6c339e21c0c069498b557b55c781ddabe1f
20192170 <e.weldeghebreal@student.tue.nl>
2020-10-14 16:09:36 +0200

    End_door
    A door is made that returns the player to the start scene

---
+++ Skyscraper Waves/Assets/Standard Assets/2D/Scripts/End.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.tag =="Player"){
+                TheEnd();
+            }
+    }
+
+    void TheEnd(){
+        Debug.Log("Done");
+        SceneManager.LoadScene (sceneName:"StartMenu");
+        }
+}
