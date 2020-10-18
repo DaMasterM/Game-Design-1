3f655d7a4f691cd01ff549654a92325d18093a04
Wouter <43830356+kabouter1123@users.noreply.github.com>
2020-10-18 15:34:10 +0200

    added restart option after level complete added death screen, made player scale down after reaching goal

--- Skyscraper Waves/Assets/Scripts/EndScoreScript.cs
+++ Skyscraper Waves/Assets/Scripts/EndScoreScript.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class EndScoreScript : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI levelStateText;
+    public Button restartButton;
+    public Button mainMenuButton;
+    private string currentLevel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +24,31 @@ public class EndScoreScript : MonoBehaviour
         {
             scoreText.text = "There has been an error computing your score.";
         }
+        if (PlayerPrefs.HasKey("levelState"))
+        {
+            levelStateText.text = PlayerPrefs.GetString("levelState");
+        }
+        restartButton.onClick.AddListener(Restart);
+        mainMenuButton.onClick.AddListener(MainMenu);
+        if (PlayerPrefs.HasKey("currentLevel"))
+        {
+            currentLevel = PlayerPrefs.GetString("currentLevel");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Restart() 
+    {
+        SceneManager.LoadScene (sceneName:currentLevel);
+    }
+
+    void MainMenu()
+    {
+        SceneManager.LoadScene (sceneName:"StartMenu");
     }
 }
--- Skyscraper Waves/Assets/Standard Assets/2D/Scripts/End.cs
+++ Skyscraper Waves/Assets/Standard Assets/2D/Scripts/End.cs
@@ -7,14 +7,16 @@ public class End : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D collision){
         UnityStandardAssets._2D.PlatformerCharacter2D player = collision.GetComponent<UnityStandardAssets._2D.PlatformerCharacter2D> ();
-        if(player.tag =="Player"){
+        if (player.tag =="Player") {
             float score = player.GetScore();
+            PlayerPrefs.SetString("levelState", "Level Completed");
             PlayerPrefs.SetFloat("score", score);
-            TheEnd(score);
+            PlayerPrefs.SetString("currentLevel", SceneManager.GetActiveScene().name);
+            Invoke("TheEnd", 0.7f);
         }
     }
 
-    void TheEnd(float score){
+    void TheEnd(){
         Debug.Log("Done");
         SceneManager.LoadScene (sceneName:"LevelComplete");
     }
