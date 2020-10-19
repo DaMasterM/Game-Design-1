7789ce22dcf31af7da3e741860f13cc04ca69922
ymkeverspui <y.r.verspui@student.tue.nl>
2020-10-19 12:12:19 +0200

    score menu

---
+++ Skyscraper Waves/Assets/Menus/HighScoreView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HighScoreView : MonoBehaviour
+{
+    public Text score1;
+    public Text score2;
+    public Text score3;
+    public Text score4;
+    public Text score5;
+
+    private void OnEnable()
+    {
+        GameObject go = GameObject.Find("System");
+        if (go != null)
+        {
+            ScoreManager sm = go.GetComponent<ScoreManager>();
+            if (sm != null && sm.Scores != null && sm.Scores.Count == 5)
+            {
+                score1.text = sm.Scores[0].ToString();
+                score2.text = sm.Scores[1].ToString();
+                score3.text = sm.Scores[2].ToString();
+                score4.text = sm.Scores[3].ToString();
+                score5.text = sm.Scores[4].ToString();
+            }
+        }
+    }
+}
